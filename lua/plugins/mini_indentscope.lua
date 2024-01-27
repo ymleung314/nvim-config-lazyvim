8a34ef6a8ed05a482467f975fc7100d13c42f651
Yau-ming Leung <ymleung314@gmail.com>
2024-01-28 01:06:15 +0800

    reduce indent scope highlight delay and remove animation

---
+++ lua/plugins/mini_indentscope.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "echasnovski/mini.indentscope",
+    opts = {
+      draw = {
+        delay = 10,
+        animation = require("mini.indentscope").gen_animation.none(),
+      },
+    },
+  },
+}
